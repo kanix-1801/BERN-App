924f4c0965fc72d2e19c5643bd7839691767e24b
kanishk singhal <kanishk.singhal1801@gmail.com>
2023-11-13 14:48:56 +0530

    contract test by api

--- BERN/api/server.js
+++ BERN/api/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const ABI = require("./ABI.json");
-const { web3 } = require("web3");
+const { Web3 } = require("web3");
 
 const app = express();
-const port = 4000;
+const PORT = 4000;
 
-app.listen(port, () => {
+const web3 = new Web3(
+  "https://polygon-mainnet.g.alchemy.com/v2/qqO6T6O2_WFhudRMLIDLrRVfjP5iGLaT"
+);
+//cotract address : 0x3C422f0e4E578472FaFDbD4621448f84270503c4
+const contractAdderss = "0x3C422f0e4E578472FaFDbD4621448f84270503c4";
+const contract = new web3.eth.Contract(ABI, contractAdderss);
+console.log(contract);
+app.listen(PORT, () => {
   console.log("start");
 });
